8a22ba4ac4cd8c630ccf4e8bd5c96d8387f9ea61
Mick F <mick@mickf.net>
2020-02-14 10:22:42 +0100

    feat: add tempate for a Quick spec file

---
+++ Custom-Templates/QuickSpec.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,15 @@
+@testable import client_ios
+import Nimble
+import Quick
+
+class ___FILEBASENAMEASIDENTIFIER___: QuickSpec {
+    override func spec() {
+        describe("something") {
+            context("when this") {
+                it("does that") {
+                    expect(hello).to(equal(world))
+                }
+            }
+        }
+    }
+}
