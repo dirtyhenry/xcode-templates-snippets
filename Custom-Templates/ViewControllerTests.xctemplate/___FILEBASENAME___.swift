9b894b48ff9426688d3b47db4ff6dde3b7e2a389
Mick F <mick@mickf.net>
2020-02-14 10:22:28 +0100

    feat: add template for a view controller test class

---
+++ Custom-Templates/ViewControllerTests.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,11 @@
+@testable import client_ios
+import SnapshotTesting
+import XCTest
+
+class ___FILEBASENAMEASIDENTIFIER___: XCTestCase {
+    func testSnapshot() {
+        record = true
+        let sut = ___FILEBASENAMEASIDENTIFIER___()
+        assertLocalizedSnapshot(matching: sut)
+    }
+}
