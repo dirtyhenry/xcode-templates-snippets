72a39286f38fe2b95dca7532fe099f520586594d
Mickaël Floc’hlay <mickael.flochlay@margo.com>
2020-03-20 12:39:08 +0100

    feat: add new templates for view and view tests

---
+++ Custom-Templates/View.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+/// TODO
+///
+/// ## Screenshots
+///
+/// ![Screenshot](../img/___FILEBASENAMEASIDENTIFIER___Tests_testDefault.1.png)
+class ___FILEBASENAMEASIDENTIFIER___: UIView {
+    // MARK: - Creating a ___FILEBASENAMEASIDENTIFIER___
+
+    init() {
+        super.init(frame: .zero)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Inspecting the foo
+}
